254d01fd39b16c6973e93f37b32092ebd8ea4f86
Ericsb52 <broadbent52@aol.com>
2023-08-21 14:15:49 -0600

    first push

---
+++ intro2.2.py
@@ -0,0 +1,9 @@
+# Eric Broadbent
+# 8/23
+# starter Program 
+
+
+print("Hello my name is Eric")
+print("I love teaching Programming")
+print("cheese cake is my favorite food")
+
---
+++ intro2.py
@@ -0,0 +1,4 @@
+print("Hello my name is Eric")
+print("I love teaching Programming")
+print("cheese cake is my favorite food")
+input()
